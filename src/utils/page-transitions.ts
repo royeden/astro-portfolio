7d718bff8aec738ae49fa49fa5834168255ebb1e
RoyEden <roy.ede@gmail.com>
2022-10-10 13:26:01 -0300

    Prevent cumulative layout shift on first page load

--- src/utils/page-transitions.ts
+++ src/utils/page-transitions.ts
@@ -2,32 +2,37 @@ import { IS_IN_CLIENT, REDUCED_MOTION } from "~constants/mediaQueries";
 
 const main = IS_IN_CLIENT && (document.querySelector("main") as HTMLElement);
 
+export const TRANSITION_PARAM_KEY = "transition";
+
 export function pageTransitionIn() {
-  main.animate(
-    [
-      { opacity: 0, height: 0 },
-      { opacity: 0, height: `${main.scrollHeight}px` },
-      { opacity: 1, height: "auto" },
-    ],
-    {
-      duration: Math.min(Math.max(500 * (main.scrollHeight / 2500), 500), 800),
-      easing: "ease-in",
-      fill: "forwards",
-    }
-  );
+  const search = new URLSearchParams(window.location.search);
+  if (search.get(TRANSITION_PARAM_KEY)) {
+    main.animate(
+      [
+        { opacity: 0, height: 0 },
+        { opacity: 0, height: `${main.scrollHeight}px` },
+        { opacity: 1, height: "auto" },
+      ],
+      {
+        duration: Math.min(Math.max(500 * (main.scrollHeight / 2500), 500), 800),
+        easing: "ease-in",
+        fill: "forwards",
+      }
+    );
+  }
 }
 
 let pageOutAnimation: Animation | undefined = undefined;
 
 function pageTransitionOut(event: MouseEvent) {
   if (!REDUCED_MOTION) {
-    const href = (event.currentTarget as HTMLAnchorElement).href;
+    const { href, pathname } = (event.currentTarget as HTMLAnchorElement);
 
     event.preventDefault();
 
     window.scrollTo(0, 0);
 
-    if (window.location.href !== href) {
+    if (window.location.pathname !== pathname) {
       if (pageOutAnimation) pageOutAnimation.cancel();
 
       pageOutAnimation = main.animate(
@@ -45,7 +50,7 @@ function pageTransitionOut(event: MouseEvent) {
         }
       );
 
-      pageOutAnimation.onfinish = () => (window.location.href = href);
+      pageOutAnimation.onfinish = () => (window.location.href = `${href}?${TRANSITION_PARAM_KEY}=true`);
     }
   }
 }
